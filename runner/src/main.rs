db419bf0bc8b90a993b21d2c2e31c1fb2502b89b
Nick Hirakawa <nickhirakawa@gmail.com>
2021-06-06 19:42:52 -0400

    2019 day 1

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -17,6 +17,14 @@ fn main() -> Result<(), AdventOfCodeError> {
                     .validator(is_day_of_month),
             ),
         )
+        .subcommand(
+            SubCommand::with_name("2019").arg(
+                Arg::with_name("day")
+                    .index(1)
+                    .takes_value(false)
+                    .validator(is_day_of_month),
+            ),
+        )
         .get_matches();
 
     if matches.is_present("2020") {
@@ -26,6 +34,14 @@ fn main() -> Result<(), AdventOfCodeError> {
             run_2020_day(day)?;
         } else {
             run_2020()?;
+        }
+    } else if matches.is_present("2019") {
+        let matches = matches.subcommand_matches("2019").unwrap();
+
+        if let Some(day) = matches.value_of("day").and_then(|s| s.parse::<u8>().ok()) {
+            run_2019_day(day)?;
+        } else {
+            run_2019()?;
         }
     } else {
         run_2020()?;
@@ -76,101 +92,118 @@ fn run_2020_day(day: u8) -> Result<(), AdventOfCodeError> {
         _ => panic!(),
     }?;
 
-    log_result(day, result);
+    log_result(2020, day, result);
 
     Ok(())
 }
 
 fn run_2020() -> Result<(), AdventOfCodeError> {
     let day_one = advent_of_code_2020::day_one::run()?;
-    log_result(1, day_one);
+    log_result(2020, 1, day_one);
 
     let day_two = advent_of_code_2020::day_two::run()?;
-    log_result(2, day_two);
+    log_result(2020, 2, day_two);
 
     let day_three = advent_of_code_2020::day_three::run()?;
-    log_result(3, day_three);
+    log_result(2020, 3, day_three);
 
     let day_four = advent_of_code_2020::day_four::run()?;
-    log_result(4, day_four);
+    log_result(2020, 4, day_four);
 
     let day_five = advent_of_code_2020::day_five::run()?;
-    log_result(5, day_five);
+    log_result(2020, 5, day_five);
 
     let day_six = advent_of_code_2020::day_six::run()?;
-    log_result(6, day_six);
+    log_result(2020, 6, day_six);
 
     let day_seven = advent_of_code_2020::day_seven::run()?;
-    log_result(7, day_seven);
+    log_result(2020, 7, day_seven);
 
     let day_eight = advent_of_code_2020::day_eight::run()?;
-    log_result(8, day_eight);
+    log_result(2020, 8, day_eight);
 
     let day_nine = advent_of_code_2020::day_nine::run()?;
-    log_result(9, day_nine);
+    log_result(2020, 9, day_nine);
 
     let day_ten = advent_of_code_2020::day_ten::run()?;
-    log_result(10, day_ten);
+    log_result(2020, 10, day_ten);
 
     let day_eleven = advent_of_code_2020::day_eleven::run()?;
-    log_result(11, day_eleven);
+    log_result(2020, 11, day_eleven);
 
     let day_twelve = advent_of_code_2020::day_twelve::run()?;
-    log_result(12, day_twelve);
+    log_result(2020, 12, day_twelve);
 
     let day_thirteen = advent_of_code_2020::day_thirteen::run()?;
-    log_result(13, day_thirteen);
+    log_result(2020, 13, day_thirteen);
 
     let day_fourteen = advent_of_code_2020::day_fourteen::run()?;
-    log_result(14, day_fourteen);
+    log_result(2020, 14, day_fourteen);
 
     let day_fifteen = advent_of_code_2020::day_fifteen::run()?;
-    log_result(15, day_fifteen);
+    log_result(2020, 15, day_fifteen);
 
     let day_sixteen = advent_of_code_2020::day_sixteen::run()?;
-    log_result(16, day_sixteen);
+    log_result(2020, 16, day_sixteen);
 
     let day_seventeen = advent_of_code_2020::day_seventeen::run()?;
-    log_result(17, day_seventeen);
+    log_result(2020, 17, day_seventeen);
 
     let day_eighteen = advent_of_code_2020::day_eighteen::run()?;
-    log_result(18, day_eighteen);
+    log_result(2020, 18, day_eighteen);
 
     let day_nineteen = advent_of_code_2020::day_nineteen::run()?;
-    log_result(19, day_nineteen);
+    log_result(2020, 19, day_nineteen);
 
     let day_twenty = advent_of_code_2020::day_twenty::run()?;
-    log_result(20, day_twenty);
+    log_result(2020, 20, day_twenty);
 
     let day_twenty_one = advent_of_code_2020::day_twenty_one::run()?;
-    log_result(21, day_twenty_one);
+    log_result(2020, 21, day_twenty_one);
 
     let day_twenty_two = advent_of_code_2020::day_twenty_two::run()?;
-    log_result(22, day_twenty_two);
+    log_result(2020, 22, day_twenty_two);
 
     let day_twenty_three = advent_of_code_2020::day_twenty_three::run()?;
-    log_result(23, day_twenty_three);
+    log_result(2020, 23, day_twenty_three);
 
     let day_twenty_four = advent_of_code_2020::day_twenty_four::run()?;
-    log_result(24, day_twenty_four);
+    log_result(2020, 24, day_twenty_four);
 
     let day_twenty_five = advent_of_code_2020::day_twenty_five::run()?;
-    log_result(25, day_twenty_five);
+    log_result(2020, 25, day_twenty_five);
 
     Ok(())
 }
 
-fn log_result(day: u8, answers: (PartAnswer, PartAnswer)) {
+fn run_2019_day(day: u8) -> Result<(), AdventOfCodeError> {
+    let result = match day {
+        1 => advent_of_code_2019::day_one::run()?,
+        _ => panic!(),
+    };
+
+    log_result(2019, day, result);
+
+    Ok(())
+}
+
+fn run_2019() -> Result<(), AdventOfCodeError> {
+    Ok(())
+}
+
+fn log_result(year: u32, day: u8, answers: (PartAnswer, PartAnswer)) {
     let (part_one, part_two) = answers;
 
     println!(
-        "day {}, part 1: {} ({:?} ms)",
+        "year {}, day {}, part 1: {} ({:?} ms)",
+        year,
         day,
         part_one.get_answer(),
         part_one.get_duration().as_millis()
     );
     println!(
-        "day {}, part 2: {} ({:?} ms)",
+        "year {}, day {}, part 2: {} ({:?} ms)",
+        year,
         day,
         part_two.get_answer(),
         part_two.get_duration().as_millis()
