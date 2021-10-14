3840d934dd8d6d02fd1f0820d62b94fe20c45a4b
Nick Hirakawa <nickhirakawa@gmail.com>
2021-10-14 16:24:34 -0400

    Solve 2018, day 3 (#12)

--- advent-of-code-2018/src/lib.rs
+++ advent-of-code-2018/src/lib.rs
@@ -1,11 +1,12 @@
 mod day_one;
+mod day_three;
 mod day_two;
 
 use common::prelude::*;
 use common::result_logger;
 
 pub fn run_all() -> Result<(), AdventOfCodeError> {
-    for i in 1..=2 {
+    for i in 1..=3 {
         run_day(i)?;
     }
 
@@ -16,6 +17,7 @@ pub fn run_day(day: u8) -> Result<(), AdventOfCodeError> {
     let result = match day {
         1 => day_one::run()?,
         2 => day_two::run()?,
+        3 => day_three::run()?,
         _ => panic!("unimplemented"),
     };
 
