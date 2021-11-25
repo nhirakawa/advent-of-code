f050bb208dec00656fd0028aa0a5525af05b9d65
Nick Hirakawa <nickhirakawa@gmail.com>
2021-11-24 20:39:31 -0500

    setup

---
+++ advent-of-code-2018/src/day_six.rs
@@ -0,0 +1,17 @@
+use common::prelude::*;
+
+pub fn run() -> AdventOfCodeResult {
+    let input = include_str!("../input/day-6.txt");
+    let part_one = part_one();
+    let part_two = part_two();
+
+    Ok((part_one, part_two))
+}
+
+fn part_one() -> PartAnswer {
+    PartAnswer::default()
+}
+
+fn part_two() -> PartAnswer {
+    PartAnswer::default()
+}
--- advent-of-code-2018/src/lib.rs
+++ advent-of-code-2018/src/lib.rs
@@ -1,6 +1,7 @@
 mod day_five;
 mod day_four;
 mod day_one;
+mod day_six;
 mod day_three;
 mod day_two;
 
@@ -8,7 +9,7 @@ use common::prelude::*;
 use common::result_logger;
 
 pub fn run_all() -> Result<(), AdventOfCodeError> {
-    for i in 1..=5 {
+    for i in 1..=6 {
         run_day(i)?;
     }
 
@@ -22,6 +23,7 @@ pub fn run_day(day: u8) -> Result<(), AdventOfCodeError> {
         3 => day_three::run()?,
         4 => day_four::run()?,
         5 => day_five::run()?,
+        6 => day_six::run()?,
         _ => panic!("unimplemented"),
     };
 
