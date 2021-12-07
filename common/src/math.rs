2e93fe9d5b95cd0edc684f63512903468220e438
Nick Hirakawa <nickhirakawa@gmail.com>
2021-12-07 11:03:00 -0500

    Solve 2021, day 7

--- common/src/math.rs
+++ common/src/math.rs
@@ -1,3 +1,5 @@
+use std::ops::{Add, Div};
+
 pub fn gcd<I: Into<i128>>(a: I, b: I) -> i128 {
     let a = a.into();
     let b = b.into();
@@ -22,6 +24,37 @@ pub fn lcm<I: Into<i128>>(a: I, b: I) -> i128 {
     (a * b) / gcd
 }
 
+pub fn median<I>(numbers: &[I]) -> f64
+where
+    I: Add<I> + Div<I> + Ord + Clone + Into<f64>,
+{
+    let mut sorted: Vec<I> = numbers.into_iter().map(|n| n.clone()).collect();
+    sorted.sort();
+
+    let midpoint = sorted.len() / 2;
+    println!("len {}, midpoint {}", sorted.len(), midpoint);
+
+    if sorted.len() % 2 == 0 {
+        (sorted[midpoint].clone().into() + sorted[midpoint - 1].clone().into()) / 2.0
+    } else {
+        sorted[midpoint].clone().into()
+    }
+}
+
+pub fn average<I>(numbers: &[I]) -> f64
+where
+    I: Add<I, Output = I> + Div<I, Output = I> + Default + Into<f64> + Copy,
+{
+    let mut sum = I::default();
+    let mut count = 0;
+    for number in numbers {
+        sum = sum + *number;
+        count += 1;
+    }
+
+    sum.into() / count as f64
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -37,5 +70,17 @@ mod tests {
     fn test_lcm() {
         assert_eq!(lcm(21, 6), 42);
         assert_eq!(lcm(lcm(8, 9), 21), 504);
+    }
+
+    #[test]
+    fn test_median() {
+        assert_eq!(median(&vec![1, 2, 3]), 2.0);
+        assert_eq!(median(&vec![1, 2, 3, 4]), 2.5);
+    }
+
+    #[test]
+    fn test_average() {
+        assert_eq!(average(&vec![4, 5, 6]), 5.0);
+        assert_eq!(average(&vec![16, 1, 2, 0, 4, 2, 7, 1, 2, 14]), 4.9);
     }
 }
