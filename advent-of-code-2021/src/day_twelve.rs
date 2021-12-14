9cb19d01689373e65e49ed296eab959588a91803
Nick Hirakawa <nickhirakawa@gmail.com>
2021-12-13 20:13:50 -0500

    solve part 1

--- advent-of-code-2021/src/day_twelve.rs
+++ advent-of-code-2021/src/day_twelve.rs
@@ -1,9 +1,14 @@
-use std::collections::{HashMap, HashSet, VecDeque};
+use std::{
+    collections::{HashMap, HashSet, VecDeque},
+    fmt::Display,
+};
 
 use common::prelude::*;
+use log::debug;
 use nom::{
     branch::alt,
-    bytes::complete::{tag, take_while},
+    bytes::complete::{tag, take_while1},
+    character::complete::multispace0,
     combinator::{all_consuming, into, map, value},
     multi::separated_list1,
     sequence::{separated_pair, terminated},
@@ -11,14 +16,26 @@ use nom::{
 };
 
 pub fn run() -> AdventOfCodeResult {
-    let part_one = part_one();
+    let input = include_str!("../input/day-12.txt");
+    let graph = parse_adjacency_list(input);
+
+    let part_one = part_one(&graph);
     let part_two = part_two();
 
     Ok((part_one, part_two))
 }
 
-fn part_one() -> PartAnswer {
-    PartAnswer::default()
+fn part_one(graph: &AdjacencyList) -> PartAnswer {
+    let start = SystemTime::now();
+    let all_paths = search(graph);
+
+    for path in &all_paths {
+        debug!("{}", path);
+    }
+
+    let number_of_paths = all_paths.len();
+
+    PartAnswer::new(number_of_paths, start.elapsed().unwrap())
 }
 
 fn part_two() -> PartAnswer {
@@ -32,11 +49,17 @@ fn search(graph: &AdjacencyList) -> Vec<Path> {
     let mut complete_paths = Vec::new();
 
     while !queue.is_empty() {
-        let next_path = queue.pop_back().unwrap();
-        for neighbors in &graph.graph[&next_path.last_vertex] {
-            todo!()
+        let next_path = queue.pop_front().unwrap();
+
+        if next_path.is_complete() {
+            complete_paths.push(next_path);
+        } else {
+            for neighbor in &graph.graph[&next_path.last_vertex] {
+                if let Some(with_neighbor) = next_path.with_vertex(neighbor) {
+                    queue.push_back(with_neighbor);
+                }
+            }
         }
-        todo!()
     }
 
     complete_paths
@@ -61,10 +84,28 @@ impl Path {
         self.last_vertex == Vertex::End
     }
 
-    fn add_vertex(&self, vertex: &Vertex) -> Option<Path> {
+    fn with_vertex(&self, vertex: &Vertex) -> Option<Path> {
+        if self.last_vertex == Vertex::End {
+            return None;
+        }
+
         match vertex {
             Vertex::Start => None,
-            Vertex::End => None,
+            Vertex::End => {
+                let mut vertices = self.vertices.clone();
+                vertices.push(Vertex::End);
+
+                let mut seen = self.seen.clone();
+                seen.insert(Vertex::End);
+
+                let last_vertex = Vertex::End;
+
+                Some(Path {
+                    vertices,
+                    seen,
+                    last_vertex,
+                })
+            }
             Vertex::SmallCave(_) => {
                 if self.seen.contains(vertex) {
                     None
@@ -102,6 +143,16 @@ impl Path {
     }
 }
 
+impl Display for Path {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for vertex in &self.vertices {
+            write!(f, "{} ", vertex)?;
+        }
+
+        Ok(())
+    }
+}
+
 #[derive(Debug, PartialEq)]
 struct AdjacencyList {
     graph: HashMap<Vertex, HashSet<Vertex>>,
@@ -132,12 +183,24 @@ enum Vertex {
     LargeCave(String),
 }
 
+impl Display for Vertex {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s = match &self {
+            Vertex::Start => "start",
+            Vertex::End => "end",
+            Vertex::SmallCave(name) | Vertex::LargeCave(name) => name,
+        };
+
+        write!(f, "{}", s)
+    }
+}
+
 fn parse_adjacency_list(i: &str) -> AdjacencyList {
     adjacency_list(i).unwrap().1
 }
 
 fn adjacency_list(i: &str) -> IResult<&str, AdjacencyList> {
-    into(all_consuming(terminated(edges, tag("\n"))))(i)
+    into(all_consuming(terminated(edges, multispace0)))(i)
 }
 
 fn edges(i: &str) -> IResult<&str, Vec<(Vertex, Vertex)>> {
@@ -161,13 +224,73 @@ fn end(i: &str) -> IResult<&str, Vertex> {
 }
 
 fn small_cave(i: &str) -> IResult<&str, Vertex> {
-    map(take_while(|c| ('a'..='z').contains(&c)), |small: &str| {
+    map(take_while1(|c| ('a'..='z').contains(&c)), |small: &str| {
         Vertex::SmallCave(small.to_string())
     })(i)
 }
 
 fn large_cave(i: &str) -> IResult<&str, Vertex> {
-    map(take_while(|c| ('A'..='Z').contains(&c)), |small: &str| {
-        Vertex::SmallCave(small.to_string())
+    map(take_while1(|c| ('A'..='Z').contains(&c)), |large: &str| {
+        Vertex::LargeCave(large.to_string())
     })(i)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_path_add_vertex() {
+        let path = Path::new();
+
+        let next_vertex = Vertex::SmallCave("asdf".to_string());
+
+        let next_path = path.with_vertex(&next_vertex);
+        assert!(next_path.is_some());
+
+        let next_path = next_path.unwrap();
+        assert!(next_path.seen.contains(&next_vertex));
+        assert!(next_path.seen.contains(&Vertex::Start));
+        assert_eq!(next_path.last_vertex, next_vertex);
+
+        assert!(next_path.with_vertex(&Vertex::Start).is_none());
+
+        assert!(next_path
+            .with_vertex(&Vertex::SmallCave("asdf".to_string()))
+            .is_none());
+    }
+
+    #[test]
+    fn test_small_cave() {
+        assert_eq!(
+            vertex("asdf"),
+            Ok(("", Vertex::SmallCave("asdf".to_string())))
+        );
+    }
+
+    #[test]
+    fn test_large_cave() {
+        assert_eq!(
+            vertex("ASDF"),
+            Ok(("", Vertex::LargeCave("ASDF".to_string())))
+        );
+    }
+
+    #[test]
+    fn test_start() {
+        assert_eq!(vertex("start"), Ok(("", Vertex::Start)));
+    }
+
+    #[test]
+    fn test_end() {
+        assert_eq!(vertex("end"), Ok(("", Vertex::End)));
+    }
+
+    #[test]
+    fn test_parse_edge() {
+        assert_eq!(
+            edge("start-A"),
+            Ok(("", (Vertex::Start, Vertex::LargeCave("A".to_string()))))
+        );
+    }
 }
