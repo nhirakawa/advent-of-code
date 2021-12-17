bb69bee5a6c7548c850665d7058fa4c5484bee40
Nick Hirakawa <nickhirakawa@gmail.com>
2021-12-16 20:29:41 -0500

    Solve 2021, day 15 (#24)

--- advent-of-code-2021/src/lib.rs
+++ advent-of-code-2021/src/lib.rs
@@ -1,5 +1,6 @@
 mod day_eight;
 mod day_eleven;
+mod day_fifteen;
 mod day_five;
 mod day_four;
 mod day_fourteen;
@@ -17,7 +18,7 @@ use common::prelude::*;
 use common::result_logger;
 
 pub fn run_all() -> Result<(), AdventOfCodeError> {
-    for i in 1..=14 {
+    for i in 1..=15 {
         run_day(i)?;
     }
 
@@ -40,6 +41,7 @@ pub fn run_day(day: u8) -> Result<(), AdventOfCodeError> {
         12 => day_twelve::run()?,
         13 => day_thirteen::run()?,
         14 => day_fourteen::run()?,
+        15 => day_fifteen::run()?,
         _ => panic!("unimplemented"),
     };
 
