bc9a49979d03601a5f4ac9baf1e367d842f6498a
Nick Hirakawa <nickhirakawa@gmail.com>
2021-12-18 20:55:56 -0500

    impl add

--- advent-of-code-2021/src/day_eighteen.rs
+++ advent-of-code-2021/src/day_eighteen.rs
@@ -17,22 +17,46 @@ fn part_two() -> PartAnswer {
     PartAnswer::default()
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Clone)]
 struct Pair {
     left: SnailfishNumber,
     right: SnailfishNumber,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Clone)]
 enum SnailfishNumber {
     Regular(u32),
     Pair(Box<Pair>),
 }
 
-impl Add for &Pair {
+impl Add for SnailfishNumber {
     type Output = Pair;
 
     fn add(self, rhs: Self) -> Self::Output {
-        todo!()
+        Pair {
+            left: self,
+            right: rhs,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add_snailfish_number() {
+        let left = SnailfishNumber::Regular(1);
+        let right = SnailfishNumber::Regular(2);
+
+        assert_eq!(left.clone() + right.clone(), Pair { left, right });
+
+        let left = SnailfishNumber::Regular(3);
+        let right = SnailfishNumber::Pair(Box::new(Pair {
+            left: SnailfishNumber::Regular(4),
+            right: SnailfishNumber::Regular(5),
+        }));
+
+        assert_eq!(left.clone() + right.clone(), Pair { left, right });
     }
 }
