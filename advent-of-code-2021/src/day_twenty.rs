6b9e7e3e6403c396c0ca1f0645af41bdddd679f5
Nick Hirakawa <nickhirakawa@gmail.com>
2021-12-21 18:41:40 -0500

    start parser

--- advent-of-code-2021/src/day_twenty.rs
+++ advent-of-code-2021/src/day_twenty.rs
@@ -1,4 +1,13 @@
+use std::collections::HashMap;
+
 use common::prelude::*;
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    combinator::{map, value},
+    multi::{many1, separated_list1},
+    IResult,
+};
 
 pub fn run() -> AdventOfCodeResult {
     let part_one = part_one();
@@ -13,4 +22,35 @@ fn part_one() -> PartAnswer {
 
 fn part_two() -> PartAnswer {
     PartAnswer::default()
+}
+
+struct ScannerOutput {
+    image_input: HashMap<(usize, usize), Pixel>,
+    enhancement_algorithm: Vec<Pixel>,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Pixel {
+    Dark,
+    Light,
+}
+
+fn image_input(i: &str) -> IResult<&str, HashMap<(usize, usize), Pixel>> {
+    map(separated_list1(tag("\n"), many1(pixel)), |rows| todo!())(i)
+}
+
+fn image_enhancement_algorithm(i: &str) -> IResult<&str, Vec<Pixel>> {
+    many1(pixel)(i)
+}
+
+fn pixel(i: &str) -> IResult<&str, Pixel> {
+    alt((dark, light))(i)
+}
+
+fn dark(i: &str) -> IResult<&str, Pixel> {
+    value(Pixel::Dark, tag("."))(i)
+}
+
+fn light(i: &str) -> IResult<&str, Pixel> {
+    value(Pixel::Light, tag("#"))(i)
 }
