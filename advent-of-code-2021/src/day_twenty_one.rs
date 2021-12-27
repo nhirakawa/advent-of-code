6c5a0c0545bdb6278edbe6d1876f9df6501a898f
Nick Hirakawa <nickhirakawa@gmail.com>
2021-12-26 20:23:03 -0500

    commented solution

--- advent-of-code-2021/src/day_twenty_one.rs
+++ advent-of-code-2021/src/day_twenty_one.rs
@@ -3,7 +3,7 @@ use log::{debug, trace};
 
 pub fn run() -> AdventOfCodeResult {
     let part_one = part_one(9, 6);
-    let part_two = part_two();
+    let part_two = part_two(9, 6);
 
     Ok((part_one, part_two))
 }
@@ -85,7 +85,47 @@ fn get_next_space(current_position: u32, spaces_forward: u32) -> u32 {
     }
 }
 
-fn part_two() -> PartAnswer {
+fn part_two(player_one_position: u8, player_two_position: u8) -> PartAnswer {
+    let mut dice = Vec::new();
+    for i in 1..4 {
+        for j in 1..4 {
+            for k in 1..4 {
+                dice.push(i + j + k);
+            }
+        }
+    }
+    /*
+      ll = [int(x.split(": ")[1]) for x in open(inf).read().strip().split('\n')]
+
+    dice = list(Counter(
+        i + j + k
+        for i in range(1, 4)
+        for j in range(1, 4)
+        for k in range(1, 4)
+    ).items())
+
+    universes = {(0, ll[0], 0, ll[1]): 1}
+    p1wins = 0
+    p2wins = 0
+    while universes:
+        nuv = defaultdict(int)
+        for state, cnt in list(universes.items()):
+            score1, pos1, score2, pos2 = state
+            for d, dcount in dice:
+                p1 = (pos1 + d - 1) % 10 + 1
+                s1 = score1 + p1
+                if s1 >= 21:
+                    p1wins += cnt * dcount
+                    continue
+                for d2, d2count in dice:
+                    p2 = (pos2 + d2 - 1) % 10 + 1
+                    s2 = score2 + p2
+                    if s2 >= 21:
+                        p2wins += cnt * dcount * d2count
+                        continue
+                    nuv[(s1, p1, s2, p2)] += cnt * dcount * d2count
+        universes = nuv
+       */
     PartAnswer::default()
 }
 
