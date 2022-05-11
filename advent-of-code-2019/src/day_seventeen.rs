5d2bad4f07aa3655ab0785674e9cf92f737b5472
Nick Hirakawa <nickhirakawa@gmail.com>
2022-05-11 12:18:04 -0400

    input

--- advent-of-code-2019/src/day_seventeen.rs
+++ advent-of-code-2019/src/day_seventeen.rs
@@ -3,6 +3,8 @@ use common::prelude::AdventOfCodeResult;
 use common::prelude::*;
 
 pub fn run() -> AdventOfCodeResult {
+    let input = include_str!("../input/day-17.txt");
+
     let part_one = part_one();
     let part_two = part_two();
 
