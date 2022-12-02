1829afe10d9ac5c1b01d483d4c0c025ef8604d4d
Nick Hirakawa <nickhirakawa@gmail.com>
2022-12-02 13:09:22 -0500

    attempt at part 2

--- advent-of-code-2022/src/day_two.rs
+++ advent-of-code-2022/src/day_two.rs
@@ -1,7 +1,11 @@
 use common::prelude::*;
 use nom::{
-    branch::alt, bytes::complete::tag, combinator::map, multi::separated_list1,
-    sequence::separated_pair, IResult,
+    branch::alt,
+    bytes::complete::tag,
+    combinator::{into, map, value},
+    multi::separated_list1,
+    sequence::separated_pair,
+    IResult,
 };
 
 pub fn run() -> AdventOfCodeResult {
@@ -15,125 +19,149 @@ pub fn run() -> AdventOfCodeResult {
     Ok((part_one, part_two))
 }
 
-fn part_one(strategy_guide: &[Strategy]) -> PartAnswer {
+fn part_one(strategy_guide: &[Round]) -> PartAnswer {
     let start = SystemTime::now();
 
-    let score: u32 = strategy_guide.iter().map(Strategy::score).sum();
+    let score: u32 = strategy_guide.iter().map(|s| score(&s.them, &s.me)).sum();
 
     let elapsed = start.elapsed().unwrap();
 
     PartAnswer::new(score, elapsed)
 }
 
-fn part_two(strategy_guide: &[Strategy]) -> PartAnswer {
-    PartAnswer::default()
+fn part_two(strategy_guide: &[Round]) -> PartAnswer {
+    let start = SystemTime::now();
+
+    let score: u32 = strategy_guide
+        .iter()
+        .map(|r| score(&r.them, &get_choice_for_outcome(&r.them, &r.outcome)))
+        .sum();
+
+    let elapsed = start.elapsed().unwrap();
+
+    // 16194 is too high
+    PartAnswer::new(score, elapsed)
 }
 
-struct Strategy {
-    them: char,
-    me: char,
+struct Round {
+    them: Choice,
+    me: Choice,
+    outcome: Outcome,
 }
 
-impl Strategy {
-    fn score(&self) -> u32 {
-        let shape_selected_score = match self.me {
-            'X' => 1,
-            'Y' => 2,
-            'Z' => 3,
-            _ => unreachable!(),
-        };
-
-        let outcome_score = match self.outcome() {
-            Outcome::Win => 6,
-            Outcome::Draw => 3,
-            Outcome::Loss => 0,
-        };
-
-        shape_selected_score + outcome_score
-    }
-
-    fn outcome(&self) -> Outcome {
-        if self.them == 'A' && self.me == 'X'
-            || self.them == 'B' && self.me == 'Y'
-            || self.them == 'C' && self.me == 'Z'
-        {
-            Outcome::Draw
-        } else {
-            let did_i_win = match self.them {
-                'A' => self.me == 'Y',
-                'B' => self.me == 'Z',
-                'C' => self.me == 'X',
-                _ => unreachable!(),
-            };
-
-            if did_i_win {
-                Outcome::Win
-            } else {
-                Outcome::Loss
-            }
-        }
+impl From<(Choice, (Choice, Outcome))> for Round {
+    fn from(raw: (Choice, (Choice, Outcome))) -> Round {
+        let (them, (me, outcome)) = raw;
+        Round { them, me, outcome }
     }
 }
 
-#[derive(Debug, PartialEq, Eq)]
+fn get_choice_for_outcome(them: &Choice, outcome: &Outcome) -> Choice {
+    match outcome {
+        Outcome::Loss => them.win(),
+        Outcome::Draw => them.draw(),
+        Outcome::Win => them.loss(),
+    }
+}
+
+fn score(them: &Choice, me: &Choice) -> u32 {
+    let outcome_score = if me.is_win(them) {
+        6
+    } else if me.is_draw(them) {
+        3
+    } else {
+        0
+    };
+
+    let choice_score = match me {
+        Choice::Rock => 1,
+        Choice::Paper => 2,
+        Choice::Scissors => 3,
+    };
+
+    outcome_score + choice_score
+}
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
 enum Outcome {
     Win,
     Loss,
     Draw,
 }
 
-fn parse(i: &str) -> Vec<Strategy> {
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+enum Choice {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+impl Choice {
+    fn is_win(&self, other: &Self) -> bool {
+        self.win() == *other
+    }
+
+    fn is_draw(&self, other: &Self) -> bool {
+        self.draw() == *other
+    }
+
+    fn is_loss(&self, other: &Self) -> bool {
+        self.loss() == *other
+    }
+
+    fn win(&self) -> Choice {
+        match self {
+            Choice::Rock => Choice::Paper,
+            Choice::Paper => Choice::Scissors,
+            Choice::Scissors => Choice::Rock,
+        }
+    }
+
+    fn draw(&self) -> Choice {
+        *self
+    }
+
+    fn loss(&self) -> Choice {
+        match self {
+            Choice::Rock => Choice::Scissors,
+            Choice::Paper => Choice::Rock,
+            Choice::Scissors => Choice::Paper,
+        }
+    }
+}
+
+fn parse(i: &str) -> Vec<Round> {
     finish(strategy_guide)(i).unwrap().1
 }
 
-fn strategy_guide(i: &str) -> IResult<&str, Vec<Strategy>> {
+fn strategy_guide(i: &str) -> IResult<&str, Vec<Round>> {
     separated_list1(tag("\n"), strategy)(i)
 }
 
-fn strategy(i: &str) -> IResult<&str, Strategy> {
-    map(separated_pair(them, tag(" "), me), |(them, me)| Strategy {
-        them,
-        me,
-    })(i)
+fn strategy(i: &str) -> IResult<&str, Round> {
+    into(separated_pair(them, tag(" "), me))(i)
 }
 
-fn them(i: &str) -> IResult<&str, char> {
-    map(alt((tag("A"), tag("B"), tag("C"))), |s: &str| {
-        s.chars().next().unwrap()
-    })(i)
+fn them(i: &str) -> IResult<&str, Choice> {
+    alt((rock, paper, scissors))(i)
 }
 
-fn me(i: &str) -> IResult<&str, char> {
-    map(alt((tag("X"), tag("Y"), tag("Z"))), |s: &str| {
-        s.chars().next().unwrap()
-    })(i)
+fn me(i: &str) -> IResult<&str, (Choice, Outcome)> {
+    alt((
+        value((Choice::Rock, Outcome::Loss), tag("X")),
+        value((Choice::Paper, Outcome::Draw), tag("Y")),
+        value((Choice::Scissors, Outcome::Win), tag("Z")),
+    ))(i)
 }
 
-#[cfg(test)]
-mod test {
-    use super::*;
+fn rock(i: &str) -> IResult<&str, Choice> {
+    value(Choice::Rock, alt((tag("A"), tag("X"))))(i)
+}
 
-    #[test]
-    fn test_did_i_win() {
-        let strategy = Strategy { them: 'A', me: 'Y' };
-        assert_eq!(strategy.outcome(), Outcome::Win);
+fn paper(i: &str) -> IResult<&str, Choice> {
+    value(Choice::Paper, alt((tag("B"), tag("Y"))))(i)
+}
 
-        let strategy = Strategy { them: 'B', me: 'X' };
-        assert_eq!(strategy.outcome(), Outcome::Loss);
-
-        let strategy = Strategy { them: 'C', me: 'Z' };
-        assert_eq!(strategy.outcome(), Outcome::Draw);
-    }
-
-    #[test]
-    fn test_score() {
-        let strategy = Strategy { them: 'A', me: 'Y' };
-        assert_eq!(strategy.score(), 8);
-
-        let strategy = Strategy { them: 'B', me: 'X' };
-        assert_eq!(strategy.score(), 1);
-
-        let strategy = Strategy { them: 'C', me: 'Z' };
-        assert_eq!(strategy.score(), 6);
-    }
+fn scissors(i: &str) -> IResult<&str, Choice> {
+    value(Choice::Scissors, alt((tag("C"), tag("Z"))))(i)
 }
