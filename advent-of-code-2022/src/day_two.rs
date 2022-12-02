623ec4f63fa0c038f9544ad9dc27a372c6b17b96
Nick Hirakawa <nickhirakawa@gmail.com>
2022-12-02 14:56:46 -0500

    Solve part 2

--- advent-of-code-2022/src/day_two.rs
+++ advent-of-code-2022/src/day_two.rs
@@ -22,7 +22,7 @@ pub fn run() -> AdventOfCodeResult {
 fn part_one(strategy_guide: &[Round]) -> PartAnswer {
     let start = SystemTime::now();
 
-    let score: u32 = strategy_guide.iter().map(|s| score(&s.them, &s.me)).sum();
+    let score: u32 = strategy_guide.iter().map(score_part_one).sum();
 
     let elapsed = start.elapsed().unwrap();
 
@@ -32,29 +32,11 @@ fn part_one(strategy_guide: &[Round]) -> PartAnswer {
 fn part_two(strategy_guide: &[Round]) -> PartAnswer {
     let start = SystemTime::now();
 
-    let score: u32 = strategy_guide
-        .iter()
-        .map(|r| score(&r.them, &get_choice_for_outcome(&r.them, &r.outcome)))
-        .sum();
+    let score: u32 = strategy_guide.iter().map(score_part_two).sum();
 
     let elapsed = start.elapsed().unwrap();
 
-    // 16194 is too high
-    //  9072 is too low
     PartAnswer::new(score, elapsed)
-}
-
-struct Round {
-    them: Choice,
-    me: Choice,
-    outcome: Outcome,
-}
-
-impl From<(Choice, (Choice, Outcome))> for Round {
-    fn from(raw: (Choice, (Choice, Outcome))) -> Round {
-        let (them, (me, outcome)) = raw;
-        Round { them, me, outcome }
-    }
 }
 
 fn get_choice_for_outcome(them: &Choice, outcome: &Outcome) -> Choice {
@@ -63,6 +45,17 @@ fn get_choice_for_outcome(them: &Choice, outcome: &Outcome) -> Choice {
         Outcome::Draw => them.draw(),
         Outcome::Win => them.win(),
     }
+}
+
+fn score_part_one(round: &Round) -> u32 {
+    score(&round.them, &round.me)
+}
+
+fn score_part_two(round: &Round) -> u32 {
+    score(
+        &round.them,
+        &get_choice_for_outcome(&round.them, &round.outcome),
+    )
 }
 
 fn score(them: &Choice, me: &Choice) -> u32 {
@@ -83,6 +76,19 @@ fn score(them: &Choice, me: &Choice) -> u32 {
     outcome_score + choice_score
 }
 
+struct Round {
+    them: Choice,
+    me: Choice,
+    outcome: Outcome,
+}
+
+impl From<(Choice, (Choice, Outcome))> for Round {
+    fn from(raw: (Choice, (Choice, Outcome))) -> Round {
+        let (them, (me, outcome)) = raw;
+        Round { them, me, outcome }
+    }
+}
+
 #[derive(Debug, PartialEq, Eq, Copy, Clone)]
 enum Outcome {
     Win,
@@ -99,15 +105,11 @@ enum Choice {
 
 impl Choice {
     fn is_win(&self, other: &Self) -> bool {
-        self.win() == *other
+        self.loss() == *other
     }
 
     fn is_draw(&self, other: &Self) -> bool {
         self.draw() == *other
-    }
-
-    fn is_loss(&self, other: &Self) -> bool {
-        self.loss() == *other
     }
 
     fn win(&self) -> Choice {
@@ -177,5 +179,73 @@ mod tests {
             get_choice_for_outcome(&Choice::Rock, &Outcome::Win),
             Choice::Paper
         );
+
+        assert_eq!(
+            get_choice_for_outcome(&Choice::Rock, &Outcome::Draw),
+            Choice::Rock
+        );
+
+        assert_eq!(
+            get_choice_for_outcome(&Choice::Rock, &Outcome::Loss),
+            Choice::Scissors
+        );
+
+        assert_eq!(
+            get_choice_for_outcome(&Choice::Paper, &Outcome::Win),
+            Choice::Scissors
+        );
+
+        assert_eq!(
+            get_choice_for_outcome(&Choice::Paper, &Outcome::Draw),
+            Choice::Paper
+        );
+
+        assert_eq!(
+            get_choice_for_outcome(&Choice::Paper, &Outcome::Loss),
+            Choice::Rock
+        );
+
+        assert_eq!(
+            get_choice_for_outcome(&Choice::Scissors, &Outcome::Win),
+            Choice::Rock
+        );
+
+        assert_eq!(
+            get_choice_for_outcome(&Choice::Scissors, &Outcome::Draw),
+            Choice::Scissors
+        );
+
+        assert_eq!(
+            get_choice_for_outcome(&Choice::Scissors, &Outcome::Loss),
+            Choice::Paper
+        );
+    }
+
+    #[test]
+    fn test_is_win() {
+        let choice = Choice::Scissors;
+        assert_eq!(choice.is_win(&Choice::Paper), true);
+        assert_eq!(choice.is_win(&Choice::Rock), false);
+        assert_eq!(choice.is_win(&choice), false);
+    }
+
+    #[test]
+    fn test_score_part_one() {}
+
+    #[test]
+    fn test_score_part_two() {
+        let round = Round {
+            them: Choice::Rock,
+            me: Choice::Paper,
+            outcome: Outcome::Draw,
+        };
+        assert_eq!(score_part_two(&round), 4);
+
+        let round = Round {
+            them: Choice::Paper,
+            me: Choice::Rock,
+            outcome: Outcome::Loss,
+        };
+        assert_eq!(score_part_two(&round), 1);
     }
 }
