fe7aca0ce5b8308c0c4ff2f1611e2f11cdfb9429
Nick Hirakawa <nickhirakawa@gmail.com>
2022-12-08 12:17:24 -0500

    solve part 2

--- advent-of-code-2022/src/day_eight.rs
+++ advent-of-code-2022/src/day_eight.rs
@@ -15,7 +15,7 @@ pub fn run() -> AdventOfCodeResult {
     let grid = parse(input);
 
     let part_one = part_one(&grid);
-    let part_two = part_two();
+    let part_two = part_two(&grid);
 
     Ok((part_one, part_two))
 }
@@ -36,11 +36,19 @@ fn part_one(grid: &Grid) -> PartAnswer {
     PartAnswer::new(count, elapsed)
 }
 
-fn part_two() -> PartAnswer {
+fn part_two(grid: &Grid) -> PartAnswer {
     let start = SystemTime::now();
 
+    let answer = grid
+        .all_coordinates()
+        .iter()
+        .map(|c| grid.score_visible_trees(c))
+        .max()
+        .unwrap_or(0);
+
     let elapsed = start.elapsed().unwrap();
-    PartAnswer::default()
+
+    PartAnswer::new(answer, elapsed)
 }
 #[derive(Debug, PartialEq, Eq, Clone)]
 struct Grid {
@@ -61,18 +69,50 @@ impl Grid {
 
         all_coordinates
     }
+
+    fn get_coordinates_up(&self, coordinate: &(usize, usize)) -> Vec<(usize, usize)> {
+        let (x, y) = *coordinate;
+
+        (0..y).rev().map(|check_y| (x, check_y)).collect()
+    }
+
+    fn get_coordinates_right(&self, coordinate: &(usize, usize)) -> Vec<(usize, usize)> {
+        let (x, y) = *coordinate;
+        (x + 1..=self.max_x).map(|check_x| (check_x, y)).collect()
+    }
+
+    fn get_coordinates_down(&self, coordinate: &(usize, usize)) -> Vec<(usize, usize)> {
+        let (x, y) = *coordinate;
+
+        (y + 1..=self.max_y).map(|check_y| (x, check_y)).collect()
+    }
+
+    fn get_coordinates_left(&self, coordinate: &(usize, usize)) -> Vec<(usize, usize)> {
+        let (x, y) = *coordinate;
+
+        (0..x).rev().map(|check_x| (check_x, y)).collect()
+    }
+
+    fn is_edge(&self, coordinate: &(usize, usize)) -> bool {
+        let (x, y) = *coordinate;
+        if x == 0 || x == self.max_x || y == 0 || y == self.max_y {
+            true
+        } else {
+            false
+        }
+    }
+
     fn is_visible(&self, coordinate: &(usize, usize)) -> bool {
         debug!("Checking {:?}", coordinate);
         let current_value = *self.heights_by_coordinate.get(coordinate).unwrap();
 
-        let (x, y) = *coordinate;
-
-        if x == 0 || x == self.max_x || y == 0 || y == self.max_y {
+        if self.is_edge(coordinate) {
             return true;
         }
 
-        let max_to_left = (0..x)
-            .map(|check_x| (check_x, y))
+        let max_to_left = self
+            .get_coordinates_left(coordinate)
+            .iter()
             .filter_map(|c| self.heights_by_coordinate.get(&c))
             .max()
             .cloned()
@@ -84,8 +124,9 @@ impl Grid {
             return true;
         }
 
-        let max_to_right = (x + 1..=self.max_x)
-            .map(|check_x| (check_x, y))
+        let max_to_right = self
+            .get_coordinates_right(coordinate)
+            .iter()
             .filter_map(|c| self.heights_by_coordinate.get(&c))
             .max()
             .cloned()
@@ -97,8 +138,9 @@ impl Grid {
             return true;
         }
 
-        let max_above = (0..y)
-            .map(|check_y| (x, check_y))
+        let max_above = self
+            .get_coordinates_up(coordinate)
+            .iter()
             .filter_map(|c| self.heights_by_coordinate.get(&c))
             .max()
             .cloned()
@@ -110,8 +152,9 @@ impl Grid {
             return true;
         }
 
-        let max_below = (y + 1..=self.max_y)
-            .map(|check_y| (x, check_y))
+        let max_below = self
+            .get_coordinates_down(coordinate)
+            .iter()
             .filter_map(|c| self.heights_by_coordinate.get(&c))
             .max()
             .cloned()
@@ -124,6 +167,60 @@ impl Grid {
         }
 
         false
+    }
+
+    fn score_visible_trees(&self, coordinate: &(usize, usize)) -> usize {
+        let current_height = *self.heights_by_coordinate.get(coordinate).unwrap();
+
+        debug!("checking {:?}, height {current_height}", coordinate);
+
+        if self.is_edge(coordinate) {
+            return 0;
+        }
+
+        let visible_trees_up =
+            self.get_visible_tree_count(&self.get_coordinates_up(coordinate), current_height);
+
+        debug!("visible_trees_up {visible_trees_up}");
+
+        let visible_trees_right =
+            self.get_visible_tree_count(&self.get_coordinates_right(coordinate), current_height);
+
+        debug!("visible_trees_right {visible_trees_right}");
+
+        let visible_trees_down =
+            self.get_visible_tree_count(&self.get_coordinates_down(coordinate), current_height);
+
+        debug!("visible_trees_down {visible_trees_down}");
+
+        let visible_trees_left =
+            self.get_visible_tree_count(&self.get_coordinates_left(coordinate), current_height);
+
+        debug!("visible_trees_left {visible_trees_left}");
+
+        visible_trees_up * visible_trees_right * visible_trees_down * visible_trees_left
+    }
+
+    fn get_visible_tree_count(
+        &self,
+        coordinates: &[(usize, usize)],
+        current_height: usize,
+    ) -> usize {
+        let mut count = 0;
+
+        for coordinate in coordinates {
+            if let Some(height) = self.heights_by_coordinate.get(coordinate) {
+                count += 1;
+
+                if *height >= current_height {
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+
+        count
     }
 }
 
@@ -183,9 +280,13 @@ fn height(i: &str) -> IResult<&str, usize> {
 mod tests {
     use super::*;
 
+    fn build_grid() -> Grid {
+        parse("30373\n25512\n65332\n33549\n35390")
+    }
+
     #[test]
     fn test_is_visible() {
-        let grid = parse("30373\n25512\n65332\n33549\n35390");
+        let grid = build_grid();
 
         assert_eq!(grid.is_visible(&(1, 1)), true); // top-left 5
         assert_eq!(grid.is_visible(&(2, 1)), true); // top-middle 5
@@ -195,5 +296,53 @@ mod tests {
 
         assert_eq!(grid.is_visible(&(0, 1)), true); // edge
         assert_eq!(grid.is_visible(&(2, 4)), true); // edge
+    }
+
+    #[test]
+    fn test_score_visible_trees() {
+        let grid = build_grid();
+
+        assert_eq!(grid.score_visible_trees(&(2, 1)), 4);
+        assert_eq!(grid.score_visible_trees(&(2, 3)), 8);
+    }
+
+    #[test]
+    fn test_get_coordinates_up() {
+        let grid = build_grid();
+
+        assert_eq!(
+            grid.get_coordinates_up(&(2, 3)),
+            vec![(2, 2), (2, 1), (2, 0)]
+        );
+    }
+
+    #[test]
+    fn test_get_coordinates_right() {
+        let grid = build_grid();
+
+        assert_eq!(
+            grid.get_coordinates_right(&(1, 1)),
+            vec![(2, 1), (3, 1), (4, 1)]
+        );
+    }
+
+    #[test]
+    fn test_get_coordinates_down() {
+        let grid = build_grid();
+
+        assert_eq!(
+            grid.get_coordinates_down(&(1, 0)),
+            vec![(1, 1), (1, 2), (1, 3), (1, 4)]
+        );
+    }
+
+    #[test]
+    fn test_get_coordinates_left() {
+        let grid = build_grid();
+
+        assert_eq!(
+            grid.get_coordinates_left(&(4, 4)),
+            vec![(3, 4), (2, 4), (1, 4), (0, 4)]
+        );
     }
 }
