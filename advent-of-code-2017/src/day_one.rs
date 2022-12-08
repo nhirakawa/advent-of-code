994686aff7f82d9cd08a3fc4c8d782f6f90667bc
Nick Hirakawa <nickhirakawa@gmail.com>
2022-12-07 21:11:48 -0500

    solve part 1

--- advent-of-code-2017/src/day_one.rs
+++ advent-of-code-2017/src/day_one.rs
@@ -1,16 +1,43 @@
+use common::parse::unsigned_number;
 use common::prelude::*;
+use nom::{bytes::complete::take, multi::many1, IResult};
 
 pub fn run() -> AdventOfCodeResult {
-    let part_one = part_one();
+    let input = include_str!("../input/day-1.txt").trim();
+
+    let digits = parse(input);
+
+    let part_one = part_one(&digits);
     let part_two = part_two();
 
     Ok((part_one, part_two))
 }
 
-fn part_one() -> PartAnswer {
-    PartAnswer::default()
+fn part_one(digits: &[u32]) -> PartAnswer {
+    let start = SystemTime::now();
+
+    let mut sum = 0;
+
+    for index in 0..digits.len() {
+        let first = digits[index];
+        let second = digits[(index + 1) % digits.len()];
+
+        if first == second {
+            sum += first;
+        }
+    }
+
+    let elapsed = start.elapsed().unwrap();
+
+    PartAnswer::new(sum, elapsed)
 }
 
 fn part_two() -> PartAnswer {
     PartAnswer::default()
+}
+
+fn parse(i: &str) -> Vec<u32> {
+    i.chars()
+        .filter_map(|d| d.to_string().parse::<u32>().ok())
+        .collect()
 }
