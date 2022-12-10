0a538f3854d7a838a15832a0ea1ab0651656fad6
Nick Hirakawa <nickhirakawa@gmail.com>
2022-12-10 11:15:28 -0500

    solve part 2

--- advent-of-code-2022/src/day_ten.rs
+++ advent-of-code-2022/src/day_ten.rs
@@ -14,7 +14,7 @@ pub fn run() -> AdventOfCodeResult {
     let operations = parse(input);
 
     let part_one = part_one(&operations);
-    let part_two = part_two();
+    let part_two = part_two(&operations);
 
     Ok((part_one, part_two))
 }
@@ -35,11 +35,32 @@ fn part_one(operations: &[Operation]) -> PartAnswer {
     PartAnswer::new(answer, elapsed)
 }
 
-fn part_two() -> PartAnswer {
+fn part_two(operations: &[Operation]) -> PartAnswer {
     let start = SystemTime::now();
 
+    let mut cpu = Cpu::new();
+
+    for operation in operations {
+        cpu.apply(operation);
+    }
+
+    let image = cpu
+        .pixels
+        .chunks_exact(40)
+        .map(|v| {
+            v.iter()
+                .map(|c| c.to_string())
+                .collect::<Vec<String>>()
+                .join("")
+        })
+        .collect::<Vec<String>>()
+        .join("\n");
+
+    let image = "\n".to_string() + &image + "\n";
+
     let elapsed = start.elapsed().unwrap();
-    PartAnswer::default()
+
+    PartAnswer::new(image, elapsed)
 }
 
 #[derive(Debug)]
@@ -47,6 +68,8 @@ struct Cpu {
     current_cycle: usize,
     current_value: isize,
     signal_strength_values: Vec<isize>,
+    pixels: Vec<char>,
+    crt_position: usize,
 }
 
 impl Cpu {
@@ -54,11 +77,15 @@ impl Cpu {
         let current_cycle = 1;
         let current_value = 1;
         let signal_strength_values = vec![];
+        let pixels = vec![];
+        let crt_position = 0;
 
         Cpu {
             current_cycle,
             current_value,
             signal_strength_values,
+            pixels,
+            crt_position,
         }
     }
 
@@ -87,11 +114,30 @@ impl Cpu {
                 || self.current_cycle == 180
                 || self.current_cycle == 220
             {
-                println!("cycle {}, value {}", self.current_cycle, self.current_value);
+                // println!("cycle {}, value {}", self.current_cycle, self.current_value);
 
                 self.signal_strength_values
                     .push(self.current_cycle as isize * self.current_value);
             }
+
+            let pixel_value = if ((self.crt_position as isize % 40) - self.current_value).abs() <= 1
+            {
+                // one of the three pixel sprites is under the CRT beam
+                solid_square()
+            } else {
+                empty_square()
+            };
+
+            // if self.current_cycle <= 21 {
+            //     println!(
+            //         "[current_cycle={:02}|crt_position={:02}|current_value={:02}] {pixel_value}",
+            //         self.current_cycle, self.crt_position, self.current_value
+            //     );
+            // }
+
+            self.pixels.push(pixel_value);
+
+            self.crt_position += 1;
 
             self.current_cycle += 1;
         }
--- common/src/prelude.rs
+++ common/src/prelude.rs
@@ -1,3 +1,4 @@
 pub use crate::answer::*;
+pub use crate::constants::*;
 pub use crate::parse::*;
 pub use std::time::{Duration, SystemTime};
