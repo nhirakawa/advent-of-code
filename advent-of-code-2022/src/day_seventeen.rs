9cacf9b2412e8d7f3207f776387797c11fc03354
Nick Hirakawa <nickhirakawa@gmail.com>
2022-12-20 22:01:17 -0500

    small working solution

--- advent-of-code-2022/src/day_seventeen.rs
+++ advent-of-code-2022/src/day_seventeen.rs
@@ -1,20 +1,345 @@
+use std::collections::HashSet;
+
 use common::prelude::*;
+use nom::{branch::alt, bytes::complete::tag, combinator::value, multi::many1, IResult};
+
+/**
+ * NOTES
+ *
+ * I use a coordinate system where
+ * - x takes on a value [0, 6] and increases right
+ * - y takes on a value [0, infinity) and increases up
+ *
+ * Spaces (0, 0) to (0, 6) can be occupied by rocks; everything else is out of bounds
+ *
+ * One optimization to make is to only keep the highest rock particle for each x position
+ * The rocks below cannot (as of part 1) influence a falling rock
+ */
 
 pub fn run() -> AdventOfCodeResult {
-    let part_one = part_one();
-    let part_two = part_two();
+    let input = include_str!("../input/day-17.txt");
+
+    let part_one = part_one(input);
+    let part_two = part_two(input);
 
     Ok((part_one, part_two))
 }
 
-fn part_one() -> PartAnswer {
+fn part_one(input: &str) -> PartAnswer {
+    let start = SystemTime::now();
+
+    let wind_directions = parse(input);
+
+    let mut game = TetrisGame::new(wind_directions);
+
+    for _ in 0..2022 {
+        game.add_rock();
+    }
+
+    let elapsed = start.elapsed().unwrap();
+
+    PartAnswer::new(game.highest_y, elapsed)
+}
+
+fn part_two(_input: &str) -> PartAnswer {
     let start = SystemTime::now();
     let _elapsed = start.elapsed().unwrap();
     PartAnswer::default()
 }
 
-fn part_two() -> PartAnswer {
-    let start = SystemTime::now();
-    let _elapsed = start.elapsed().unwrap();
-    PartAnswer::default()
+#[derive(Debug, PartialEq, Eq, Clone)]
+struct TetrisGame {
+    occupied_cells: HashSet<(isize, isize)>,
+    highest_y: isize,
+    next_shape_type: ShapeType,
+    wind_directions: Vec<WindDirection>,
+    current_wind_index: usize,
+}
+
+impl TetrisGame {
+    fn new(wind_directions: Vec<WindDirection>) -> TetrisGame {
+        let occupied_cells = HashSet::new();
+        let highest_y = 0;
+        let next_shape_type = ShapeType::initial();
+        let current_wind_index = 0;
+
+        TetrisGame {
+            occupied_cells,
+            highest_y,
+            next_shape_type,
+            wind_directions,
+            current_wind_index,
+        }
+    }
+
+    fn add_rock(&mut self) {
+        println!("Dropping {:?}", self.next_shape_type);
+        let current_rock: Shape = self.next_shape_type.into();
+        let mut current_rock = current_rock.with_delta((2, self.highest_y + 4));
+
+        loop {
+            let wind_direction = self.next_direction();
+
+            println!("-- wind is blowing {:?}", wind_direction);
+
+            current_rock = self.blow_with_wind(&current_rock, wind_direction);
+
+            if self.has_settled(&current_rock) {
+                println!("-- rock has settled");
+                break;
+            }
+
+            println!("-- rock is moving down");
+            current_rock = current_rock.with_delta((0, -1));
+        }
+
+        self.occupied_cells.extend(current_rock.offsets);
+        self.highest_y = self.occupied_cells.iter().map(|(_, y)| *y).max().unwrap();
+        println!("-- new highest y is {}", self.highest_y);
+        self.next_shape_type = self.next_shape_type.next();
+    }
+
+    fn next_direction(&mut self) -> WindDirection {
+        let wind_direction = self.wind_directions.get(self.current_wind_index).unwrap();
+
+        self.current_wind_index = (self.current_wind_index + 1) % self.wind_directions.len();
+
+        *wind_direction
+    }
+
+    fn blow_with_wind(&self, shape: &Shape, direction: WindDirection) -> Shape {
+        let (min_x, max_x) = shape.min_max_x();
+
+        if min_x == 0 && direction == WindDirection::Left {
+            return shape.clone();
+        }
+
+        if max_x == 6 && direction == WindDirection::Right {
+            return shape.clone();
+        }
+
+        match direction {
+            WindDirection::Left => {
+                if min_x == 0 {
+                    shape.clone()
+                } else {
+                    let would_collide = shape
+                        .offsets
+                        .iter()
+                        .map(|(x, y)| (*x - 1, *y))
+                        .any(|cell| self.occupied_cells.contains(&cell));
+
+                    if would_collide {
+                        shape.clone()
+                    } else {
+                        shape.with_delta((-1, 0))
+                    }
+                }
+            }
+            WindDirection::Right => {
+                if max_x == 6 {
+                    shape.clone()
+                } else {
+                    let would_collide = shape
+                        .offsets
+                        .iter()
+                        .map(|(x, y)| (*x + 1, *y))
+                        .any(|cell| self.occupied_cells.contains(&cell));
+
+                    if would_collide {
+                        shape.clone()
+                    } else {
+                        shape.with_delta((1, 0))
+                    }
+                }
+            }
+        }
+    }
+
+    fn has_settled(&self, shape: &Shape) -> bool {
+        if shape.offsets.iter().any(|(_, y)| *y == 1) {
+            println!("---- rock has settled at bottom");
+            return true;
+        }
+
+        let settled = shape
+            .offsets
+            .iter()
+            .map(|(x, y)| (*x, *y - 1))
+            .any(|cell| self.occupied_cells.contains(&cell));
+
+        println!("---- settled - {settled}");
+
+        settled
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+enum WindDirection {
+    Left,
+    Right,
+}
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+enum ShapeType {
+    Horizontal,
+    Cross,
+    El,
+    Vertical,
+    Square,
+}
+
+impl ShapeType {
+    fn initial() -> ShapeType {
+        ShapeType::Horizontal
+    }
+
+    fn next(&self) -> ShapeType {
+        match self {
+            ShapeType::Horizontal => ShapeType::Cross,
+            ShapeType::Cross => ShapeType::El,
+            ShapeType::El => ShapeType::Vertical,
+            ShapeType::Vertical => ShapeType::Square,
+            ShapeType::Square => ShapeType::Horizontal,
+        }
+    }
+
+    fn get_height(&self) -> isize {
+        match self {
+            ShapeType::Horizontal => 1,
+            ShapeType::Cross => 3,
+            ShapeType::El => 3,
+            ShapeType::Vertical => 4,
+            ShapeType::Square => 2,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+struct Shape {
+    offsets: Vec<(isize, isize)>,
+    bottom_edge: Vec<(isize, isize)>,
+    height: isize,
+}
+
+impl Shape {
+    fn new(offsets: Vec<(isize, isize)>, bottom_edge: Vec<(isize, isize)>) -> Shape {
+        let mut min_y = isize::MAX;
+        let mut max_y = isize::MIN;
+
+        for (_, y) in &offsets {
+            min_y = min_y.min(*y);
+            max_y = max_y.max(*y);
+        }
+
+        let height = max_y - min_y;
+
+        Shape {
+            offsets,
+            height,
+            bottom_edge,
+        }
+    }
+
+    fn with_delta(&self, delta: (isize, isize)) -> Shape {
+        let (delta_x, delta_y) = delta;
+        let offsets = self
+            .offsets
+            .iter()
+            .map(|(x, y)| (*x + delta_x, *y + delta_y))
+            .collect();
+
+        Shape::new(offsets, self.bottom_edge.clone())
+    }
+
+    fn min_max_x(&self) -> (isize, isize) {
+        let mut min_x = isize::MAX;
+        let mut max_x = isize::MIN;
+
+        for (x, _) in &self.offsets {
+            min_x = min_x.min(*x);
+            max_x = max_x.max(*x);
+        }
+
+        (min_x, max_x)
+    }
+}
+
+impl From<ShapeType> for Shape {
+    fn from(shape_type: ShapeType) -> Shape {
+        match shape_type {
+            ShapeType::Horizontal => Shape::new(
+                vec![(0, 0), (1, 0), (2, 0), (3, 0)],
+                vec![(0, 0), (1, 0), (2, 0), (3, 0)],
+            ),
+            ShapeType::Vertical => Shape::new(vec![(0, 0), (0, 1), (0, 2), (0, 3)], vec![(0, 0)]),
+            ShapeType::Square => {
+                Shape::new(vec![(0, 0), (1, 0), (0, 1), (1, 1)], vec![(0, 0), (1, 0)])
+            }
+            ShapeType::El => Shape::new(
+                vec![(0, 0), (1, 0), (2, 0), (2, 1), (2, 2)],
+                vec![(0, 0), (1, 0), (2, 0)],
+            ),
+            ShapeType::Cross => Shape::new(
+                vec![(1, 0), (0, 1), (1, 1), (2, 1), (1, 2)],
+                vec![(0, 1), (1, 0), (2, 1)],
+            ),
+        }
+    }
+}
+
+fn parse(i: &str) -> Vec<WindDirection> {
+    finish(wind_directions)(i).unwrap().1
+}
+
+fn wind_directions(i: &str) -> IResult<&str, Vec<WindDirection>> {
+    many1(wind_direction)(i)
+}
+
+fn wind_direction(i: &str) -> IResult<&str, WindDirection> {
+    alt((left, right))(i)
+}
+
+fn left(i: &str) -> IResult<&str, WindDirection> {
+    value(WindDirection::Left, tag("<"))(i)
+}
+
+fn right(i: &str) -> IResult<&str, WindDirection> {
+    value(WindDirection::Right, tag(">"))(i)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_game_example() {
+        let mut game = TetrisGame::new(parse(">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"));
+
+        game.add_rock();
+
+        assert_eq!(
+            game.occupied_cells,
+            vec![(2, 1), (3, 1), (4, 1), (5, 1)].into_iter().collect()
+        );
+
+        game.add_rock();
+
+        assert_eq!(
+            game.occupied_cells,
+            vec![
+                (2, 1),
+                (3, 1),
+                (4, 1),
+                (5, 1),
+                (3, 2),
+                (2, 3),
+                (3, 3),
+                (4, 3),
+                (3, 4)
+            ]
+            .into_iter()
+            .collect()
+        );
+    }
 }
