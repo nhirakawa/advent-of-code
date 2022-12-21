f23eb37fa6fdf038227992c9a034582d86bfe9e0
Nick Hirakawa <nickhirakawa@gmail.com>
2022-12-21 17:39:26 -0500

    attempt to solve part 2

--- advent-of-code-2022/src/day_eighteen.rs
+++ advent-of-code-2022/src/day_eighteen.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, VecDeque};
 
 use common::prelude::*;
 use nom::{
@@ -9,7 +9,7 @@ pub fn run() -> AdventOfCodeResult {
     let input = include_str!("../input/day-18.txt");
 
     let part_one = part_one(input);
-    let part_two = part_two();
+    let part_two = part_two(input);
 
     Ok((part_one, part_two))
 }
@@ -24,9 +24,10 @@ fn part_one(input: &str) -> PartAnswer {
     for coordinate in &coordinates {
         let neighbors = coordinate.neighbors();
 
-        let connected_neighbors = coordinates.intersection(&neighbors).count();
+        let connected_neighbors: Vec<Coordinate> =
+            coordinates.intersection(&neighbors).copied().collect();
 
-        surface_area += 6 - connected_neighbors;
+        surface_area += 6 - connected_neighbors.len();
     }
 
     let elapsed = start.elapsed().unwrap();
@@ -34,10 +35,98 @@ fn part_one(input: &str) -> PartAnswer {
     PartAnswer::new(surface_area, elapsed)
 }
 
-fn part_two() -> PartAnswer {
+fn part_two(input: &str) -> PartAnswer {
     let start = SystemTime::now();
-    let _elapsed = start.elapsed().unwrap();
-    PartAnswer::default()
+
+    let coordinates = parse(input);
+
+    let mut min_x = isize::MAX;
+    let mut max_x = isize::MIN;
+
+    let mut min_y = isize::MAX;
+    let mut max_y = isize::MIN;
+
+    let mut min_z = isize::MAX;
+    let mut max_z = isize::MIN;
+
+    for coordinate in &coordinates {
+        min_x = min_x.min(coordinate.x);
+        max_x = max_x.max(coordinate.x);
+
+        min_y = min_y.min(coordinate.y);
+        max_y = max_y.max(coordinate.y);
+
+        min_z = min_z.min(coordinate.z);
+        max_z = max_z.max(coordinate.z);
+    }
+
+    min_x = min_x - 10;
+    max_x = max_x + 10;
+
+    min_y = min_y - 10;
+    max_y = max_y + 10;
+
+    min_z = min_z - 10;
+    max_z = max_z + 10;
+
+    println!("Bounding box min ({min_x},{min_y},{min_z}), max ({max_x},{max_y},{max_z})");
+
+    let mut coordinates_to_test = HashSet::new();
+
+    let min = (min_x, min_y, min_z);
+
+    let mut queue = VecDeque::new();
+    queue.push_back(min);
+
+    let mut seen = HashSet::new();
+
+    while let Some(next) = queue.pop_front() {
+        let (x, y, z) = next;
+
+        if !seen.insert(next) {
+            continue;
+        }
+
+        if !(min_x..=max_x).contains(&x)
+            || !(min_y..=max_y).contains(&y)
+            || !(min_z..=max_z).contains(&z)
+        {
+            continue;
+        }
+
+        let coordinate = Coordinate::new(x, y, z);
+        let neighbors = coordinate.neighbors();
+
+        for neighbor in neighbors {
+            if coordinates.contains(&neighbor) {
+                coordinates_to_test.insert(neighbor);
+            } else {
+                queue.push_back((neighbor.x, neighbor.y, neighbor.z));
+            }
+        }
+    }
+
+    println!(
+        "Testing {}/{} coordinates",
+        coordinates_to_test.len(),
+        coordinates.len()
+    );
+
+    let mut surface_area = 0;
+
+    for coordinate in &coordinates_to_test {
+        let neighbors = coordinate.neighbors();
+
+        let connected_neighbors: Vec<Coordinate> =
+            coordinates.intersection(&neighbors).copied().collect();
+
+        surface_area += 6 - connected_neighbors.len();
+    }
+
+    let elapsed = start.elapsed().unwrap();
+
+    // 2621 is too high
+    PartAnswer::new(surface_area, elapsed)
 }
 
 #[derive(Debug, PartialEq, Eq, Copy, Clone, Hash)]
@@ -63,6 +152,38 @@ impl Coordinate {
         ]
         .into_iter()
         .collect()
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+struct Neighbors {
+    positive_x: Coordinate,
+    negative_x: Coordinate,
+    positive_y: Coordinate,
+    negative_y: Coordinate,
+    positive_z: Coordinate,
+    negative_z: Coordinate,
+}
+
+impl From<&Coordinate> for Neighbors {
+    fn from(coordinate: &Coordinate) -> Neighbors {
+        let positive_x = Coordinate::new(coordinate.x + 1, coordinate.y, coordinate.z);
+        let negative_x = Coordinate::new(coordinate.x - 1, coordinate.y, coordinate.z);
+
+        let positive_y = Coordinate::new(coordinate.x, coordinate.y + 1, coordinate.z);
+        let negative_y = Coordinate::new(coordinate.x, coordinate.y - 1, coordinate.z);
+
+        let positive_z = Coordinate::new(coordinate.x, coordinate.y, coordinate.z + 1);
+        let negative_z = Coordinate::new(coordinate.x, coordinate.y, coordinate.z - 1);
+
+        Neighbors {
+            positive_x,
+            negative_x,
+            positive_y,
+            negative_y,
+            positive_z,
+            negative_z,
+        }
     }
 }
 
