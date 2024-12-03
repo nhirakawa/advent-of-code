065e5654fdb66f61b2643f5c795da785d07185d5
Nick Hirakawa <nickhirakawa@gmail.com>
2024-12-03 08:16:59 -0500

    Solve 2024, day 2

--- src/year_2024.rs
+++ src/year_2024.rs
@@ -1,11 +1,14 @@
 use crate::common::base::{Day, Part};
 
 mod day_one;
+mod day_two;
 
 pub fn solution(day: Day, part: Part) -> Option<fn(&str) -> anyhow::Result<String>> {
     match (day, part) {
         (Day::Day1, Part::PartOne) => Some(day_one::part_one),
         (Day::Day1, Part::PartTwo) => Some(day_one::part_two),
+        (Day::Day2, Part::PartOne) => Some(day_two::part_one),
+        (Day::Day2, Part::PartTwo) => Some(day_two::part_two),
         _ => None,
     }
 }
---
+++ src/year_2024/day_two.rs
@@ -0,0 +1,72 @@
+use crate::common::parse::{finish, unsigned_number};
+use anyhow::anyhow;
+use nom::{bytes::complete::tag, character::complete::space1, multi::separated_list1, IResult};
+
+pub fn part_one(input: &str) -> anyhow::Result<String> {
+    let reports = parse(input)?;
+
+    let safe_report_count = reports.into_iter().filter(is_safe_part_one).count();
+
+    Ok(safe_report_count.to_string())
+}
+
+pub fn part_two(input: &str) -> anyhow::Result<String> {
+    let reports = parse(input)?;
+
+    let safe_report_count = reports.into_iter().filter(is_safe_part_two).count();
+
+    Ok(safe_report_count.to_string())
+}
+
+fn is_safe_part_one(report: &Report) -> bool {
+    let all_increasing = report.windows(2).all(|w| w[0] < w[1]);
+    if all_increasing {
+        return report.windows(2).all(|w| w[1] - w[0] <= 3);
+    }
+
+    let all_decreasing = report.windows(2).all(|w| w[0] > w[1]);
+    if all_decreasing {
+        return report.windows(2).all(|w| w[0] - w[1] <= 3);
+    }
+
+    false
+}
+
+fn is_safe_part_two(report: &Report) -> bool {
+    if is_safe_part_one(report) {
+        return true;
+    }
+
+    for idx in 0..report.len() {
+        let mut report = report.clone();
+        report.remove(idx);
+
+        if is_safe_part_one(&report) {
+            return true;
+        }
+    }
+
+    false
+}
+
+type Level = u32;
+type Report = Vec<Level>;
+type Reports = Vec<Report>;
+
+fn parse(input: &str) -> anyhow::Result<Reports> {
+    finish(reports)(input)
+        .map_err(|e| anyhow!(e.to_string()))
+        .map(|(_, reports)| reports)
+}
+
+fn reports(i: &str) -> IResult<&str, Reports> {
+    separated_list1(tag("\n"), report)(i)
+}
+
+fn report(i: &str) -> IResult<&str, Report> {
+    separated_list1(space1, level)(i)
+}
+
+fn level(i: &str) -> IResult<&str, Level> {
+    unsigned_number(i)
+}
