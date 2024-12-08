22edc66d1214c3b9f72deb3fb9c4e46332e8f8f5
Nick Hirakawa <nickhirakawa@gmail.com>
2024-12-08 13:05:28 -0500

    Solve 2016, day 3

---
+++ src/year_2016/day_three.rs
@@ -0,0 +1,31 @@
+use itertools::Itertools;
+
+pub fn part_one(input: &str) -> anyhow::Result<String> {
+    let integers = into_integers(input);
+
+    let triangles = integers
+        .tuples()
+        .filter(|(a, b, c)| *a + *b > *c && *a + *c > *b && *b + *c > *a)
+        .count();
+
+    Ok(triangles.to_string())
+}
+
+pub fn part_two(input: &str) -> anyhow::Result<String> {
+    let integers = into_integers(input);
+
+    let triangles = integers
+        .tuples()
+        .flat_map(|(a, b, c, d, e, f, g, h, i)| [(a, d, g), (b, e, h), (c, f, i)])
+        .filter(|(a, b, c)| *a + *b > *c && *a + *c > *b && *b + *c > *a)
+        .count();
+
+    Ok(triangles.to_string())
+}
+
+fn into_integers(input: &str) -> impl Iterator<Item = u32> + '_ {
+    input
+        .trim()
+        .lines()
+        .flat_map(|line| line.split_whitespace().filter_map(|s| s.parse().ok()))
+}
