532a414fd6d77832e0d712f7b31a460915439174
Nick Hirakawa <nickhirakawa@gmail.com>
2024-12-08 12:50:02 -0500

    Solve 2015, day 2

--- src/year_2015.rs
+++ src/year_2015.rs
@@ -8,11 +8,14 @@ mod day_one;
 mod day_seven;
 mod day_six;
 mod day_three;
+mod day_two;
 
 pub fn solution(day: Day, part: Part) -> Option<fn(&str) -> anyhow::Result<String>> {
     match (day, part) {
         (Day::Day1, Part::PartOne) => Some(day_one::part_one),
         (Day::Day1, Part::PartTwo) => Some(day_one::part_two),
+        (Day::Day2, Part::PartOne) => Some(day_two::part_one),
+        (Day::Day2, Part::PartTwo) => Some(day_two::part_two),
         (Day::Day3, Part::PartOne) => Some(day_three::part_one),
         (Day::Day3, Part::PartTwo) => Some(day_three::part_two),
         (Day::Day4, Part::PartOne) => Some(day_four::part_one),
---
+++ src/year_2015/day_two.rs
@@ -0,0 +1,69 @@
+use nom::{
+    bytes::complete::tag, combinator::map, multi::separated_list1, sequence::tuple, IResult,
+};
+
+use crate::common::parse::{finish, unsigned_number};
+
+pub fn part_one(input: &str) -> anyhow::Result<String> {
+    let dimensions = parse(input)?;
+
+    let mut sum = 0;
+
+    for (length, width, height) in dimensions {
+        let lw = length * width;
+        let wh = width * height;
+        let hl = height * length;
+
+        let smallest_side = lw.min(wh).min(hl);
+
+        sum += 2 * lw + 2 * wh + 2 * hl + smallest_side;
+    }
+
+    Ok(sum.to_string())
+}
+
+pub fn part_two(input: &str) -> anyhow::Result<String> {
+    let dimensions = parse(input)?;
+
+    let mut sum = 0;
+
+    for (length, width, height) in dimensions {
+        let lw_perimeter = 2 * length + 2 * width;
+        let wh_perimeter = 2 * width + 2 * height;
+        let hl_perimeter = 2 * height + 2 * length;
+
+        let smallest_perimeter = lw_perimeter.min(wh_perimeter).min(hl_perimeter);
+
+        let volume = length * width * height;
+
+        sum += smallest_perimeter + volume;
+    }
+
+    Ok(sum.to_string())
+}
+
+type Dimension = (u32, u32, u32);
+type Dimensions = Vec<Dimension>;
+
+fn parse(i: &str) -> anyhow::Result<Dimensions> {
+    finish(dimensions)(i)
+        .map(|(_, dimensions)| dimensions)
+        .map_err(|e| e.to_owned().into())
+}
+
+fn dimensions(i: &str) -> IResult<&str, Dimensions> {
+    separated_list1(tag("\n"), dimension)(i)
+}
+
+fn dimension(i: &str) -> IResult<&str, Dimension> {
+    map(
+        tuple((
+            unsigned_number,
+            tag("x"),
+            unsigned_number,
+            tag("x"),
+            unsigned_number,
+        )),
+        |(length, _, width, _, height)| (length, width, height),
+    )(i)
+}
