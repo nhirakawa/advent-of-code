4616df15241e490d428706421afb189541142767
Nick Hirakawa <nickhirakawa@gmail.com>
2025-01-11 20:41:14 -0500

    Solve 2015, day 20

--- src/year_2015/day_twenty.rs
+++ src/year_2015/day_twenty.rs
@@ -1,9 +1,41 @@
 use anyhow::bail;
 
-pub fn part_one(_input: &str) -> anyhow::Result<String> {
-    bail!("Not implemented")
+pub fn part_one(input: &str) -> anyhow::Result<String> {
+    let number_of_presents = input.trim().parse::<usize>()?;
+
+    let mut houses = vec![0; number_of_presents / 10];
+
+    for i in 1..(number_of_presents / 10) {
+        for j in (i..(number_of_presents / 10)).step_by(i) {
+            houses[j] += i * 10;
+        }
+    }
+
+    for (i, presents) in houses.iter().enumerate() {
+        if *presents >= number_of_presents {
+            return Ok(i.to_string());
+        }
+    }
+
+    bail!("No solution found")
 }
 
-pub fn part_two(_input: &str) -> anyhow::Result<String> {
-    bail!("Not implemented")
+pub fn part_two(input: &str) -> anyhow::Result<String> {
+    let number_of_presents = input.trim().parse::<usize>()?;
+
+    let mut houses = vec![0; number_of_presents / 11];
+
+    for i in 1..(number_of_presents / 11) {
+        for j in (i..(number_of_presents / 11)).step_by(i).take(50) {
+            houses[j] += i * 11;
+        }
+    }
+
+    for (i, presents) in houses.iter().enumerate() {
+        if *presents >= number_of_presents {
+            return Ok(i.to_string());
+        }
+    }
+
+    bail!("No solution found")
 }
