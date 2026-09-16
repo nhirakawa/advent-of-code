70a5f213fd50e2edbf7c8086fb13677ab2b501d5
Nick Hirakawa <nickhirakawa@gmail.com>
2026-09-16 11:45:35 -0400

    Attempt part 2

--- src/year_2019/day_twenty_two.rs
+++ src/year_2019/day_twenty_two.rs
@@ -1,3 +1,4 @@
+use crate::common::math;
 use anyhow::{anyhow, bail};
 
 pub fn part_one(input: &str) -> anyhow::Result<impl ToString> {
@@ -5,8 +6,10 @@ pub fn part_one(input: &str) -> anyhow::Result<impl ToString> {
     Ok(shuffle_fn.call(2019))
 }
 
-pub fn part_two(_input: &str) -> anyhow::Result<impl ToString> {
-    Err::<usize, _>(anyhow!("Not implemented"))
+pub fn part_two(input: &str) -> anyhow::Result<impl ToString> {
+    // 78081957803263 is too high
+    let shuffle_fn = compose_shuffle_fns(input, 119_315_717_514_047)?;
+    shuffle_fn.call_inverse(2020, 101_741_582_076_661)
 }
 
 fn compose_shuffle_fns(s: &str, deck_size: i128) -> anyhow::Result<ShuffleFn> {
@@ -44,8 +47,29 @@ impl ShuffleFn {
         }
     }
 
+    /// Finds the final location of the `n`th card after doing 1 shuffle
     fn call(&self, n: i128) -> i128 {
         (self.a * n + self.b).rem_euclid(self.deck_size)
+    }
+
+    /// Finds the card at index `n` after doing `k` shuffles
+    fn call_inverse(&self, n: i128, k: i128) -> anyhow::Result<i128> {
+        let big_a = math::mod_pow(self.a, k, self.deck_size);
+
+        let term_1 = math::mod_pow(self.a, k, self.deck_size) - 1;
+        let term_1 = term_1.rem_euclid(self.deck_size);
+
+        let inverse_denominator = math::mod_inverse(term_1 - 1, self.deck_size)
+            .ok_or(anyhow!("Modular inverse not found"))?;
+
+        let b_sum = (term_1 * inverse_denominator).rem_euclid(self.deck_size);
+
+        let big_b = (self.b * b_sum).rem_euclid(self.deck_size);
+
+        Ok(((n - big_b)
+            * math::mod_inverse(big_a, self.deck_size)
+                .ok_or(anyhow!("Modular inverse not found"))?)
+        .rem_euclid(self.deck_size))
     }
 
     /// Returns a ShuffleFn of the form other(self(x))
