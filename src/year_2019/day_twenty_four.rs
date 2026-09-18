956ffb44d854bc8db39a9d2139f49227a3f18f7e
Nick Hirakawa <nickhirakawa@gmail.com>
2026-09-18 16:52:42 -0400

    Solve 2019, day 24 (#82)

--- src/year_2019/day_twenty_four.rs
+++ src/year_2019/day_twenty_four.rs
@@ -1,9 +1,199 @@
-use anyhow::anyhow;
+use crate::common::parse::griderator;
+use anyhow::{anyhow, bail};
+use std::collections::{HashMap, HashSet};
+use std::hash::Hash;
+use std::iter::successors;
 
-pub fn part_one(_input: &str) -> anyhow::Result<impl ToString> {
-    Err::<usize, _>(anyhow!("Not implemented"))
+pub fn part_one(input: &str) -> anyhow::Result<impl ToString> {
+    let cells: HashSet<Position> = cells_from_str(input).collect();
+
+    let mut seen = HashSet::new();
+
+    for (iterations, round) in successors(Some(cells), |cells| Some(step(cells))).enumerate() {
+        if iterations >= 10_000 {
+            break;
+        }
+
+        let biodiversity = biodiversity(&round);
+
+        if !seen.insert(biodiversity) {
+            return Ok(biodiversity);
+        }
+    }
+
+    bail!("No solution found")
 }
 
-pub fn part_two(_input: &str) -> anyhow::Result<impl ToString> {
-    Err::<usize, _>(anyhow!("Not implemented"))
+pub fn part_two(input: &str) -> anyhow::Result<impl ToString> {
+    let cells: HashSet<RecursivePosition> = cells_from_str(input).collect();
+
+    successors(Some(cells), |cells| Some(step(cells)))
+        .nth(200)
+        .map(|cells| cells.len())
+        .ok_or(anyhow!("No solution found"))
+}
+
+fn step<C: Cell>(bugs: &HashSet<C>) -> HashSet<C> {
+    let mut neighbor_counts: HashMap<_, usize> = HashMap::new();
+
+    for bug in bugs {
+        for adjacent in bug.adjacent() {
+            *neighbor_counts.entry(adjacent).or_default() += 1;
+        }
+    }
+
+    neighbor_counts
+        .into_iter()
+        .filter(|(cell, count)| {
+            if bugs.contains(cell) {
+                *count == 1
+            } else {
+                *count == 1 || *count == 2
+            }
+        })
+        .map(|(cell, _)| cell)
+        .collect()
+}
+
+fn biodiversity(cells: &HashSet<Position>) -> u32 {
+    cells
+        .iter()
+        .fold(0, |accumulator, element| accumulator | element.as_bits())
+}
+
+trait Cell: Eq + Hash + Copy + Clone + From<(isize, isize)> {
+    fn adjacent(&self) -> Vec<Self>;
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+struct Position([isize; 2]);
+
+impl Position {
+    #[cfg(test)]
+    fn new(x: isize, y: isize) -> Self {
+        Self([x, y])
+    }
+
+    fn as_bits(&self) -> u32 {
+        // Each octet represents a row (with 3 unused bits)
+        // Each bit in an octet represents a column in that row (with 3 unused bits)
+        // Representation is little-endian
+        let [x, y] = self.0;
+        1 << ((y * 5) + x)
+    }
+}
+
+impl From<(isize, isize)> for Position {
+    fn from((x, y): (isize, isize)) -> Self {
+        Self([x, y])
+    }
+}
+
+impl Cell for Position {
+    fn adjacent(&self) -> Vec<Self> {
+        let [x, y] = self.0;
+        [
+            Position([x + 1, y]),
+            Position([x - 1, y]),
+            Position([x, y + 1]),
+            Position([x, y - 1]),
+        ]
+        .into_iter()
+        .filter(|p| (0..5).contains(&p.0[0]) && (0..5).contains(&p.0[1]))
+        .collect()
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+struct RecursivePosition([isize; 3]);
+
+impl Cell for RecursivePosition {
+    fn adjacent(&self) -> Vec<Self> {
+        let [x, y, z] = self.0;
+
+        if x == 2 && y == 2 {
+            return vec![];
+        }
+
+        let mut adjacent = Vec::new();
+
+        // Check up/right/down/left, ignoring (2, 2, z)
+        for [adjacent_x, adjacent_y] in [[x + 1, y], [x - 1, y], [x, y + 1], [x, y - 1]] {
+            if (0..5).contains(&adjacent_x)
+                && (0..5).contains(&adjacent_y)
+                && !(adjacent_x == 2 && adjacent_y == 2)
+            {
+                adjacent.push(RecursivePosition([adjacent_x, adjacent_y, z]));
+            }
+        }
+
+        if x == 0 {
+            // Add (1, 2) in the outer layer
+            adjacent.push(RecursivePosition([1, 2, z - 1]));
+        }
+        if x == 4 {
+            // Add (3, 2) in the outer layer
+            adjacent.push(RecursivePosition([3, 2, z - 1]));
+        }
+        if y == 0 {
+            // Add (2, 1) in the outer layer
+            adjacent.push(RecursivePosition([2, 1, z - 1]));
+        }
+        if y == 4 {
+            // Add (2, 3) in the outer layer
+            adjacent.push(RecursivePosition([2, 3, z - 1]));
+        }
+        if x == 2 && y == 1 {
+            // Add top row of inner layer
+            for x in 0..5 {
+                adjacent.push(RecursivePosition([x, 0, z + 1]));
+            }
+        }
+        if x == 3 && y == 2 {
+            // Add right column of inner layer
+            for y in 0..5 {
+                adjacent.push(RecursivePosition([4, y, z + 1]));
+            }
+        }
+        if x == 2 && y == 3 {
+            // Add bottom row of inner layer
+            for x in 0..5 {
+                adjacent.push(RecursivePosition([x, 4, z + 1]));
+            }
+        }
+        if x == 1 && y == 2 {
+            // Add left column of inner layer
+            for y in 0..5 {
+                adjacent.push(RecursivePosition([0, y, z + 1]));
+            }
+        }
+
+        adjacent
+    }
+}
+
+impl From<(isize, isize)> for RecursivePosition {
+    fn from((x, y): (isize, isize)) -> Self {
+        Self([x, y, 0])
+    }
+}
+
+fn cells_from_str<C: Cell>(s: &str) -> impl Iterator<Item = C> + '_ {
+    griderator(s)
+        .filter(|(_, c)| *c == '#')
+        .map(|(position, _)| position.into())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_position_as_bits() {
+        let position = Position::new(1, 3);
+        assert_eq!(position.as_bits(), 1 << 16);
+
+        let position = Position::new(4, 4);
+        assert_eq!(position.as_bits(), 1 << 24);
+    }
 }
