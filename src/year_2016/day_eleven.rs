3b3132ac633e00bbbb694bdf70315a95412c64ec
Nick Hirakawa <nickhirakawa@gmail.com>
2026-09-23 20:19:47 -0400

    Add structures

--- src/year_2016/day_eleven.rs
+++ src/year_2016/day_eleven.rs
@@ -6,3 +6,16 @@ pub fn part_one(_input: &str) -> anyhow::Result<impl ToString> {
 pub fn part_two(_input: &str) -> anyhow::Result<impl ToString> {
     Err::<usize, _>(anyhow!("not implemented"))
 }
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+enum Component {
+    Microchip(String),
+    Generator(String),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct TestingFacility {
+    elevator_floor: usize,
+    floors: [Vec<Component>; 4],
+}
+
