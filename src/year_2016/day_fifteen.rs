22900a176e4178d92cadde98f53229f3d1e20c4f
Nick Hirakawa <nickhirakawa@gmail.com>
2026-09-24 19:12:36 -0400

    Solve 2016, day 15 (#89)

--- src/year_2016/day_fifteen.rs
+++ src/year_2016/day_fifteen.rs
@@ -1,8 +1,108 @@
-use anyhow::anyhow;
+use crate::common::math;
+use crate::common::math::Congruence;
+use anyhow::{Context, anyhow, bail};
+use itertools::Itertools;
+use log::info;
+use std::str::FromStr;
 
-pub fn part_one(_input: &str) -> anyhow::Result<impl ToString> {
-    Err::<usize, _>(anyhow!("Not implemented"))
+pub fn part_one(input: &str) -> anyhow::Result<impl ToString> {
+    let discs = Discs::from_str(input)?;
+    math::chinese_remainder::<Integer, Vec<Congruence<Integer>>>(discs.into())
+        .ok_or(anyhow!("No solution found"))
 }
-pub fn part_two(_input: &str) -> anyhow::Result<impl ToString> {
-    Err::<usize, _>(anyhow!("Not implemented"))
+
+pub fn part_two(input: &str) -> anyhow::Result<impl ToString> {
+    let mut discs = Discs::from_str(input)?;
+    discs.push(11, 0)?;
+    math::chinese_remainder::<Integer, Vec<Congruence<Integer>>>(discs.into())
+        .ok_or(anyhow!("No solution found"))
+}
+
+type Integer = i64;
+
+#[derive(Debug)]
+struct Discs(Vec<Disc>);
+
+impl Discs {
+    fn push(
+        &mut self,
+        number_of_positions: Integer,
+        starting_position: Integer,
+    ) -> anyhow::Result<()> {
+        let index = self.0.len() + 1;
+        let index = index.try_into()?;
+        let disc = Disc {
+            index,
+            number_of_positions,
+            starting_position,
+        };
+        self.0.push(disc);
+        Ok(())
+    }
+}
+
+impl FromStr for Discs {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut discs = Vec::new();
+        for line in s.lines() {
+            let disc = Disc::from_str(line)?;
+            discs.push(disc);
+        }
+        Ok(Self(discs))
+    }
+}
+
+impl From<Discs> for Vec<Congruence<Integer>> {
+    fn from(val: Discs) -> Self {
+        val.0.iter().copied().map(Disc::into).collect()
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+struct Disc {
+    index: Integer,
+    number_of_positions: Integer,
+    starting_position: Integer,
+}
+
+impl FromStr for Disc {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let s = s.trim().replace(['#', ';', '=', ',', '.'], " ");
+
+        let tokens = s.split_ascii_whitespace().collect_vec();
+
+        if tokens.len() != 13 {
+            bail!("Expected 13 tokens but found {} - {tokens:?}", tokens.len());
+        }
+
+        info!("Tokens: {tokens:?}");
+
+        let index = tokens[1]
+            .parse()
+            .with_context(|| format!("Could not parse {}", tokens[1]))?;
+        let number_of_positions = tokens[3]
+            .parse()
+            .with_context(|| format!("Could not parse number of positions from {}", tokens[2]))?;
+        let starting_position = tokens[12]
+            .parse()
+            .with_context(|| format!("Could not parse starting position from {}", tokens[12]))?;
+
+        Ok(Disc {
+            index,
+            number_of_positions,
+            starting_position,
+        })
+    }
+}
+
+impl From<Disc> for Congruence<Integer> {
+    fn from(val: Disc) -> Self {
+        let residue = -(val.starting_position + val.index);
+        let modulus = val.number_of_positions;
+        Congruence { residue, modulus }
+    }
 }
