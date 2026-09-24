f0d75221815cb384be6cb494ebfb2ef7d0a3c988
Nick Hirakawa <nickhirakawa@gmail.com>
2026-09-24 13:09:23 -0400

    Solve part 1

--- src/year_2016/day_thirteen.rs
+++ src/year_2016/day_thirteen.rs
@@ -1,8 +1,88 @@
-use anyhow::anyhow;
+use anyhow::{anyhow, bail};
+use std::collections::{HashMap, HashSet, VecDeque};
+use std::str::FromStr;
 
-pub fn part_one(_input: &str) -> anyhow::Result<impl ToString> {
+pub fn part_one(input: &str) -> anyhow::Result<impl ToString> {
+    let favorite_number = usize::from_str(input)?;
+    bfs(Position(1, 1), Position(31, 39), favorite_number)
+}
+pub fn part_two(input: &str) -> anyhow::Result<impl ToString> {
+    let _favorite_number = usize::from_str(input)?;
     Err::<usize, _>(anyhow!("Not implemented"))
 }
-pub fn part_two(_input: &str) -> anyhow::Result<impl ToString> {
-    Err::<usize, _>(anyhow!("Not implemented"))
+
+fn bfs(from: Position, to: Position, favorite_number: usize) -> anyhow::Result<usize> {
+    let mut tile_cache = HashMap::new();
+    let mut seen = HashSet::new();
+
+    let mut queue = VecDeque::new();
+    queue.push_back((from, 0));
+
+    while let Some((current, distance)) = queue.pop_front() {
+        if current == to {
+            return Ok(distance);
+        }
+
+        if !seen.insert(current) {
+            continue;
+        }
+
+        for adjacent in current.adjacent() {
+            if seen.contains(&adjacent) {
+                continue;
+            }
+
+            let tile = tile_cache
+                .entry(adjacent)
+                .or_insert(tile(adjacent, favorite_number));
+
+            if *tile == Tile::Space {
+                queue.push_back((adjacent, distance + 1));
+            }
+        }
+    }
+
+    bail!("No solution found")
+}
+
+fn tile(position: Position, favorite_number: usize) -> Tile {
+    let x = position.0;
+    let y = position.1;
+
+    let value = x.pow(2) + (3 * x) + (2 * x * y) + y + y.pow(2);
+    let value = value + favorite_number;
+
+    if value.count_ones().is_multiple_of(2) {
+        Tile::Space
+    } else {
+        Tile::Wall
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+struct Position(usize, usize);
+
+impl Position {
+    fn adjacent(&self) -> Vec<Self> {
+        let mut adjacent = Vec::with_capacity(4);
+
+        adjacent.push(Position(self.0 + 1, self.1));
+        adjacent.push(Position(self.0, self.1 + 1));
+
+        if self.0 > 0 {
+            adjacent.push(Position(self.0 - 1, self.1));
+        }
+
+        if self.1 > 0 {
+            adjacent.push(Position(self.0, self.1 - 1));
+        }
+
+        adjacent
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+enum Tile {
+    Wall,
+    Space,
 }
