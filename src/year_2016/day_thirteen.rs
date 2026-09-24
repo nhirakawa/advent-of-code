39b0a04b609deae95ac7550e3624d3a2f6c630d4
Nick Hirakawa <nickhirakawa@gmail.com>
2026-09-24 13:21:18 -0400

    Solve part 2

--- src/year_2016/day_thirteen.rs
+++ src/year_2016/day_thirteen.rs
@@ -7,8 +7,8 @@ pub fn part_one(input: &str) -> anyhow::Result<impl ToString> {
     bfs(Position(1, 1), Position(31, 39), favorite_number)
 }
 pub fn part_two(input: &str) -> anyhow::Result<impl ToString> {
-    let _favorite_number = usize::from_str(input)?;
-    Err::<usize, _>(anyhow!("Not implemented"))
+    let favorite_number = usize::from_str(input)?;
+    Ok(explore(Position(1, 1), favorite_number))
 }
 
 fn bfs(from: Position, to: Position, favorite_number: usize) -> anyhow::Result<usize> {
@@ -43,6 +43,37 @@ fn bfs(from: Position, to: Position, favorite_number: usize) -> anyhow::Result<u
     }
 
     bail!("No solution found")
+}
+
+fn explore(from: Position, favorite_number: usize) -> usize {
+    let mut seen = HashSet::new();
+
+    let mut tile_cache = HashMap::new();
+
+    let mut queue = VecDeque::new();
+    queue.push_back((from, 0));
+
+    while let Some((current, distance)) = queue.pop_front() {
+        if distance > 50 {
+            continue;
+        }
+
+        if !seen.insert(current) {
+            continue;
+        }
+
+        for adjacent in current.adjacent() {
+            let tile = tile_cache
+                .entry(adjacent)
+                .or_insert_with(|| tile(adjacent, favorite_number));
+
+            if *tile == Tile::Space {
+                queue.push_back((adjacent, distance + 1));
+            }
+        }
+    }
+
+    seen.len()
 }
 
 fn tile(position: Position, favorite_number: usize) -> Tile {
